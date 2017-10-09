53d4fb680c44798b6d41348bfc4bd0abd57e83c2
Guillaume Chupin <guillaume.chup@gmail.com>
2017-10-09 21:32:09 +0200

    Ajout mineure a compareToName de Employee

--- TD2/Type de données/src/set/Employee.java
+++ TD2/Type de données/src/set/Employee.java
@@ -4,18 +4,18 @@ public class Employee implements IComparable
 {
 	private String Nom;
 	private double Salaire;
-	
-	
+
+
 	public int compareToName(Object other)
 	{
 		if (this.Nom == ((Employee)other).Nom)
 			return 0;
 		else if (this.Nom.compareTo(((Employee)other).Nom) < 0)
 			return -1;
-		return 1;
-			
+		else
+			return 1;
 	}
-	
+
 	@Override
 	public int compareTo(Object other)
 	{
@@ -24,6 +24,6 @@ public class Employee implements IComparable
 		else if (this.Salaire > ((Employee)other).Salaire)
 			return 1;
 		else
-			return -1;	
+			return -1;
 	}
 }
